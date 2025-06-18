5f78f3a53459d8a6443e264ecda75769c3d0505c
James Hatfield <james.hatfield17@icloud.com>
2025-06-18 21:55:30 +0100

    Created Main class and initial program loop

---
+++ src/bankPackage/Main.java
@@ -0,0 +1,34 @@
+package bankPackage;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please enter your name: ");
+        String name = input.nextLine();
+
+        System.out.println("Please enter your initial bank balance: ");
+        int initBalance = Integer.parseInt(input.nextLine());
+
+        var newUser = new BankAccount(name, initBalance);
+
+        while (true) {
+            System.out.println("View Balance | Deposit | Withdraw | quit");
+            String userInput = input.nextLine();
+
+            if (userInput.equalsIgnoreCase("quit")) {
+                break;
+            }
+
+            switch (userInput.toLowerCase()) {
+                case "view":
+                    newUser.getBalance();
+                case "deposit":
+                    System.out.println("How much do you want to despoit?");
+                    int depositAmount = Integer.parseInt(input.nextLine());
+                    newUser.desposit(depositAmount);
+            }
+        }
+    }
+}
