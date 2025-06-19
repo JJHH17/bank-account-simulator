e969820f8022380fb87774277986295ec8c61357
James Hatfield <james.hatfield17@icloud.com>
2025-06-19 18:49:13 +0100

    Added relevant spacing between methods

--- src/bankPackage/BankAccount.java
+++ src/bankPackage/BankAccount.java
@@ -1,19 +1,21 @@
 package bankPackage;
-import java.util.InputMismatchException;
 
 // Responsible for handling user bank account
 public class BankAccount {
     String name;
     private int balance;
 
+
     public BankAccount(String name, int balance) {
         this.name = name;
         this.balance = balance;
     }
 
+
     public void getBalance() {
         System.out.println("Current Balance: " + this.balance);
     }
+
 
     public void deposit(int value) {
         if (value < 0) {
@@ -23,15 +25,12 @@ public class BankAccount {
         }
     }
 
+
     public void withdraw(int value) {
-        try {
-            if (value > this.balance) {
-                System.out.println("Not enough funds.");
-            } else {
-                this.balance -= value;
-            }
-        } catch (InputMismatchException e) {
-            System.out.println("Please enter a valid input");
+        if (value > this.balance) {
+            System.out.println("Not enough funds.");
+        } else {
+            this.balance -= value;
         }
     }
 }
